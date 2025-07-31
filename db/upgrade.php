a0d144f326353b4410d7dc45b09ad665e479c702
MisterShady <shadeytepta@gmail.com>
2025-07-31 14:27:13 +0300

    Обновлена БД через XMLDB

---
+++ db/upgrade.php
@@ -0,0 +1,38 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+function xmldb_block_olympiads_upgrade($oldversion) {
+    global $DB;
+
+    $dbman = $DB->get_manager();
+
+      if ($oldversion < 2025073101) {
+
+        // Define table block_olympiads to be created.
+        $table = new xmldb_table('block_olympiads');
+
+        // Adding fields to table block_olympiads.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('name', XMLDB_TYPE_CHAR, '100', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('description', XMLDB_TYPE_CHAR, '1000', null, null, null, null);
+        $table->add_field('registration_start', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('registration_end', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('registration_created', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('creatorid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+
+        // Adding keys to table block_olympiads.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('creatorid', XMLDB_KEY_FOREIGN, ['creatorid'], 'user', ['id']);
+
+        // Conditionally launch create table for block_olympiads.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Olympiads savepoint reached.
+        upgrade_block_savepoint(true, 2025073101, 'olympiads');
+    }
+
+    // Everything has succeeded to here. Return true.
+    return true;
+}
--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025073100; 
+$plugin->version   = 2025073101; 
 $plugin->requires  = 2020110900;
 $plugin->component = 'block_olympiads';
 $plugin->maturity  = MATURITY_ALPHA;
